a247644f53a6d34677a3a4dbec13515b76611fa9
RitukoIsibasi0222 <ritukodayo40@gmail.com>
2026-05-06 14:55:49 +0900

    chore: backend ESLint / Prettier 設定追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(
     allowHeaders: ["Content-Type", "Authorization"],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
-  })
+  }),
 );
 
 // ヘルスチェック
